aa78c11106617a474c142301f41aa77d811c9a08
Gabriel <gabrielsirtori.correa@gmail.com>
2022-07-12 15:16:05 -0300

    Criando e testando layout HomeScreen

---
+++ RetiradaDeMateriais/src/controller/HomeScreenController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class HomeScreenController {
+
+    @FXML
+    private TableColumn<?, ?> tblColumnComponent;
+
+    @FXML
+    private TableColumn<?, ?> tblColumnDay;
+
+    @FXML
+    private TableColumn<?, ?> tblColumnName;
+
+    @FXML
+    private TableColumn<?, ?> tblColumnStatus;
+
+    @FXML
+    private TableColumn<?, ?> tblColumnTime;
+
+    @FXML
+    private TableView<?> tblLoans;
+
+    @FXML
+    void btnComponents(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHistory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearch(ActionEvent event) {
+
+    }
+
+}
