8b2279775644dd9dab3667bbcfadcb6674ff2cb0
Gabriel <gabrielsirtori.correa@gmail.com>
2022-07-15 14:28:04 -0300

    CriandoLayoutComponents

--- RetiradaDeMateriais/src/controller/ComponentsController.java
+++ RetiradaDeMateriais/src/controller/ComponentsController.java
@@ -1,5 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ComponentsController {
-    
+
+    @FXML
+    private TableColumn<?, ?> tblColumnComponent;
+
+    @FXML
+    private TableColumn<?, ?> tblColumnStatus;
+
+    @FXML
+    private TableColumn<?, ?> tblColumnTime;
+
+    @FXML
+    private TableView<?> tblLoans;
+
+    @FXML
+    void btnLoan(ActionEvent event) {
+
+    }
+
 }
