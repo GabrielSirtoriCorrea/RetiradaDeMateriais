eee7ff8bdc7f8196d9baee99ca8a2d8b9dd9195c
Gabriel <gabrielsirtori.correa@gmail.com>
2022-07-15 15:07:36 -0300

    CriandoLayoutHistory

--- RetiradaDeMateriais/src/model/App.java
+++ RetiradaDeMateriais/src/model/App.java
@@ -13,7 +13,7 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlfile = new FXMLLoader(getClass().getResource("/view/Components.fxml"));
+        FXMLLoader fxmlfile = new FXMLLoader(getClass().getResource("/view/History.fxml"));
         Parent root = fxmlfile.load();
         Scene scene = new Scene(root);
 
